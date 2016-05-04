b8be042f3b4f51c078333fec6637f56e666510d4
DABO <daboaicha14@gmail.com>
2016-05-04 21:19:32 +0200

    change 3
    Beginning of TODO1

--- app/controllers/MyDisques.php
+++ app/controllers/MyDisques.php
@@ -2,6 +2,7 @@
 use micro\controllers\Controller;
 use micro\js\Jquery;
 use micro\utils\RequestUtils;
+
 class MyDisques extends Controller{
 	public function initialize(){
 		if(!RequestUtils::isAjax()){
@@ -10,7 +11,41 @@ class MyDisques extends Controller{
 	}
 	public function index() {
 		echo Jquery::compile();
+		if (Auth::isAuth()==True) //verifier si un utilisateur est bien connecté
+			//afficher "Mes Disques->Nom de l'utilisateur "
+		$user=Auth::getUser();
+		echo "<strong> Mes Disques </strong> ->";
+        echo $user -> getLogin();
+		   //afficher les disques
+		$disques=micro\orm\DAO::getOneToMany($user, "disques");
+		$nombredisque=count($disques);
+		echo '<br/>';
+		for ($num=0; $num > $nombredisque; $num= $num + 1){
+
+		    //espace de disque uccupé.
+			$occupation=ModelUtils::getDisqueOccupation($GLOBALS["config"],$disques[$num]);
+
+			//convertir la taille en octet
+            $taille=DirectoryUtils::formatBytes($occupation);
+
+			//le pourcentage de chaque disque
+			$tarif=ModelUtils::getDisqueTarif($disques[$num]);
+			if($tarif -> getPrix()==0)
+			    {
+
+
+			}
+			else{
+
+
+			}
+		}
+
+
+	
+		}
 	}
+
 
 	public function finalize(){
 		if(!RequestUtils::isAjax()){
